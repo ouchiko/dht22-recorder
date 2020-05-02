7520d3e0585a49f57c43b3e97b87cfa3b4b7f1bd
James Holden <james_holden@londonmarketing.com>
2020-05-02 20:48:55 +0100

    Update: Readme and update to monitor

--- monitor.py
+++ monitor.py
@@ -9,7 +9,7 @@ import time
 print("Launching temperature monitoring....")
 
 # API Endpoint
-api_endpoint = "http://192.168.0.60:8085/api/v1/set"
+api_endpoint = "http://temp.webcoding.co.uk/api/v1/set"
 # Sensor definition
 dht11_sensor = Adafruit_DHT.DHT22
 # GIPO PIN number
@@ -17,13 +17,17 @@ data_pin = 4
 # debugger?
 debug = (len(sys.argv)>1 and sys.argv[1] == "debug")
 
+f = open("/var/log/temp.log","w+")
+
 # Forever!
 while True:
     try:
+        
         # Read in the humidity and temperature.
         humidity, temperature = Adafruit_DHT.read_retry(dht11_sensor, data_pin)
         if (debug):
             print("temp:",temperature,"humd:",humidity)
+            f.write("Sensor: temp:",temperature,"humd:",humidity)
         try:
             # Get the current time 
             now = datetime.datetime.now()
@@ -34,12 +38,17 @@ while True:
                 "time": now
             })
             # Make a request to the endpoint.
-            res = requests.get('', params=params);  
+            f.write("Connecting: ",api_endpoint)
+            res = requests.get(api_endpoint, params=params);  
         except:
             if (debug):
                 print("Error when connecting to endpoint")
+                f.write("Error when connecting to endpoint")
     except:
         if (debug):
             print("There was an error when getting the data")
+            f.write("There was an error when getting the data")
     time.sleep(1);
+
+f.close()
     
